5f773173f4aa8190d8f5abdcec4ab53f584efc9c
Guillem Barba <guillem@nan-tic.com>
2014-06-03 17:04:33 +0200

    Remove __main__ entry point and PATH manipulation in test

--- tests/test_file_format.py
+++ tests/test_file_format.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-
-import sys
+import os.path
 import tempfile
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT, test_view,\
@@ -99,14 +92,13 @@ class FileFormatTestCase(unittest.TestCase):
 
             self.assertEqual(file_content, (
                     '"module","model","name","N. fields","N. fields 2"\r\n'
-                    '"file_format","file.format","----File Format","14,00","14.00"\r\n'))
+                    '"file_format","file.format","----File Format","14,00",'
+                    '"14.00"\r\n'))
             os.unlink(temp_file.name)
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(FileFormatTestCase))
+    suite.addTests(
+        unittest.TestLoader().loadTestsFromTestCase(FileFormatTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
